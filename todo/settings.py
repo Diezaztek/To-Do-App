80d361e1f7e78b336c7a74fdb006e8afa0d181e5
Paco Torres <paco.tcast@gmail.com>
2020-05-10 12:28:02 -0500

    Add heroku django lib

--- todo/settings.py
+++ todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
@@ -134,3 +135,5 @@ STATICFILES_DIRS = (
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
