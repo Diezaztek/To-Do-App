5918b202733cf952aec69fce24761df973aa52b2
Paco Torres <paco.tcast@gmail.com>
2020-05-10 12:56:35 -0500

    Change middlware version

--- todo/settings.py
+++ todo/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!m!y=6mn=q=b0ney!do^#a_%ckb1s+geu&w$0*9l_z^!y3zy=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['punk-tesla.herokuapp.com']
+ALLOWED_HOSTS = ['punk-tesla.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -132,4 +130,4 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
